ba0ff86818f6880c1ffe0b6b41912d8f45c3dae0
Atty Cronin <antoniajcronin@gmail.com>
2023-07-23 13:42:58 +0100

    Implement fetchTodo using useEffect hook

--- src/components/hooks/use_effect/use_effect.tsx
+++ src/components/hooks/use_effect/use_effect.tsx
@@ -1,13 +1,35 @@
 // make an api call using useEffect
+import { useEffect, useState } from 'react';
+
+type Todo = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
 
 export const APICall = () => {
+  const [todo, setTodo] = useState<Todo | null>(null);
+  const url = 'https://jsonplaceholder.typicode.com/todos/1';
+  const fetchTodo = async () => {
+    try {
+      const response = await fetch(url);
+      const result = await response.json();
+      setTodo(result);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    // Your code here!
+  useEffect(() => {
+    fetchTodo();
+  }, []);
 
-    return (
-        <>
-            <h2>useEffect</h2>
-            <p>Your todo here!</p>
-        </>
-    )
-}
+  return (
+    <>
+      <h2>useEffect - TODO</h2>
+      <p>Title: {todo?.title}</p>
+      <p>Completed: {todo?.completed ? 'true' : 'false'}</p>
+    </>
+  );
+};
