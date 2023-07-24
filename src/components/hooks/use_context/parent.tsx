6d0b978a141d748193a3aecbf5f1cbc3dac30fe6
Atty Cronin <antoniajcronin@gmail.com>
2023-07-24 17:29:39 +0100

    Apply ThemeContext to Child components

--- src/components/hooks/use_context/parent.tsx
+++ src/components/hooks/use_context/parent.tsx
@@ -2,7 +2,7 @@ import { createContext, useState } from 'react';
 import { Child1 } from './child_1';
 import { Child2 } from './child_2';
 
-const ThemeContext = createContext(false);
+export const ThemeContext = createContext(false);
 
 export const Parent = () => {
   const [darkTheme, setDarkTheme] = useState(true);
@@ -16,8 +16,8 @@ export const Parent = () => {
       <div className='section'>
         <h2>useContext</h2>
         <button onClick={toggleTheme}>Toggle Theme</button>
-        <Child1 dark={darkTheme} />
-        <Child2 dark={darkTheme} />
+        <Child1 />
+        <Child2 />
       </div>
     </ThemeContext.Provider>
   );
