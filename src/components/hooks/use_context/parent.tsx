46046d23bcacf546eef076a61525da63ea1dd14f
Atty Cronin <antoniajcronin@gmail.com>
2023-07-24 17:42:54 +0100

    Create ThemeType and defaultTheme so createContext acepts the correct shape

--- src/components/hooks/use_context/parent.tsx
+++ src/components/hooks/use_context/parent.tsx
@@ -2,7 +2,20 @@ import { createContext, useState } from 'react';
 import { Child1 } from './child_1';
 import { Child2 } from './child_2';
 
-export const ThemeContext = createContext(false);
+type ThemeType = {
+  backgroundColor: string;
+  color: string;
+  padding: string;
+  margin: string;
+};
+
+const defaultTheme: ThemeType = {
+  backgroundColor: '#CCC',
+  color: '#333',
+  padding: '2rem',
+  margin: '2rem',
+};
+export const ThemeContext = createContext(defaultTheme);
 
 export const Parent = () => {
   const [darkTheme, setDarkTheme] = useState(true);
@@ -11,14 +24,20 @@ export const Parent = () => {
     setDarkTheme((prevDarkTheme) => !prevDarkTheme);
   };
 
+  const theme: ThemeType = {
+    backgroundColor: darkTheme ? '#333' : '#CCC',
+    color: darkTheme ? 'white' : '#333',
+    padding: '2rem',
+    margin: '2rem',
+  };
   return (
-    <ThemeContext.Provider value={darkTheme}>
-      <div className='section'>
+    <ThemeContext.Provider value={theme}>
+      <section>
         <h2>useContext</h2>
         <button onClick={toggleTheme}>Toggle Theme</button>
         <Child1 />
         <Child2 />
-      </div>
+      </section>
     </ThemeContext.Provider>
   );
 };
