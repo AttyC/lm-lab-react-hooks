14ac81e46d3cc48ad2cfc49ea8d82f32a8b09215
Atty Cronin <antoniajcronin@gmail.com>
2023-07-25 14:33:24 +0100

    Implement useRef

--- src/components/hooks/use_ref/count_renders.tsx
+++ src/components/hooks/use_ref/count_renders.tsx
@@ -1,21 +1,27 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 export const CountRenders = () => {
-	const [value, setValue] = useState('');
-	const count = 1;
+  const [value, setValue] = useState('');
 
-	return (
-		<>
-			<h2>useRef</h2>
+  const ref = useRef(1);
 
-			<input
-				value={value}
-				type='text'
-				onChange={(e) => setValue(e.target.value)}
-			/>
+  function handleClick(item: string) {
+    ref.current = ref.current + 1;
+    setValue(item);
+  }
 
-			<p>{value}</p>
-			<p>I have rendered {count} times</p>
-		</>
-	);
+  return (
+    <>
+      <h2>useRef</h2>
+
+      <input
+        value={value}
+        type='text'
+        onChange={(e) => handleClick(e.target.value)}
+      />
+
+      <p>{value}</p>
+      <p>I have rendered {ref.current} times</p>
+    </>
+  );
 };
